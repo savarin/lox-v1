6fc080bddfe6094b21cae750fd3639afe2367e89
Ezzeri Esa <ezzeri.esa@gmail.com>
2020-09-29 10:20:15 -0700

    expose integer values in debug

--- src/debug.py
+++ src/debug.py
@@ -126,5 +126,7 @@ def convert_value(val):
     """
     if isinstance(val, value.Value) and val.is_string():
         return "".join(val.as_cstring()[:-1])
+    elif isinstance(val, value.Value) and val.is_number():
+        return val.value_as
 
     return val
